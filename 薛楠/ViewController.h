9d7407a176e0cd131aaab63b8a92f0e2970c6229
Mega1 <mega1@toaddemacbook-air.local>
2016-01-28 10:06:36 +0800

    Initial Commit

---
+++ 薛楠/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  薛楠
+//
+//  Created by Mega1 on 16/1/28.
+//  Copyright © 2016年 薛楠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 薛楠/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  薛楠
+//
+//  Created by Mega1 on 16/1/28.
+//  Copyright © 2016年 薛楠. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
